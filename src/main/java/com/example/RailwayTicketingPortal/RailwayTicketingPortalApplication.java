c85b18582c2961d8269e276bae29f31106cdcc3a
Desislava Simeonova <d.simeonova@blubito.com>
2023-10-10 17:49:48 +0300

    Initial commit

---
+++ src/main/java/com/example/RailwayTicketingPortal/RailwayTicketingPortalApplication.java
@@ -0,0 +1,13 @@
+package com.example.RailwayTicketingPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayTicketingPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwayTicketingPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RailwayTicketingPortal/RailwayTicketingPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RailwayTicketingPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwayTicketingPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
